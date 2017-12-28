045754ccb7a097256e75b5bda0586e945d486429
Cory LePage <cory@razorcx.com>
2017-12-28 10:42:02 -0800

    Initial commit

---
+++ PhaseViewer/PhaseViewerForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PhaseViewer
+{
+	partial class PhaseViewerForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhaseViewerForm));
+			this.buttonShowAllPhases = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.buttonShowPhase = new System.Windows.Forms.Button();
+			this.dataGridViewPhases = new System.Windows.Forms.DataGridView();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewPhases)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonShowAllPhases
+			// 
+			this.buttonShowAllPhases.Location = new System.Drawing.Point(325, 56);
+			this.buttonShowAllPhases.Name = "buttonShowAllPhases";
+			this.buttonShowAllPhases.Size = new System.Drawing.Size(151, 33);
+			this.buttonShowAllPhases.TabIndex = 27;
+			this.buttonShowAllPhases.Text = "Show All Phases";
+			this.buttonShowAllPhases.UseVisualStyleBackColor = true;
+			this.buttonShowAllPhases.Click += new System.EventHandler(this.buttonShowAllPhases_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 74);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(55, 17);
+			this.label1.TabIndex = 26;
+			this.label1.Text = "Phases";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::PhaseViewer.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(160, 41);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 25;
+			this.pictureBox1.TabStop = false;
+			// 
+			// buttonShowPhase
+			// 
+			this.buttonShowPhase.Location = new System.Drawing.Point(325, 17);
+			this.buttonShowPhase.Name = "buttonShowPhase";
+			this.buttonShowPhase.Size = new System.Drawing.Size(151, 33);
+			this.buttonShowPhase.TabIndex = 35;
+			this.buttonShowPhase.Text = "Show Phase";
+			this.buttonShowPhase.UseVisualStyleBackColor = true;
+			this.buttonShowPhase.Click += new System.EventHandler(this.buttonShowPhase_Click);
+			// 
+			// dataGridViewPhases
+			// 
+			this.dataGridViewPhases.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+			this.dataGridViewPhases.BackgroundColor = System.Drawing.SystemColors.Window;
+			this.dataGridViewPhases.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewPhases.Location = new System.Drawing.Point(15, 95);
+			this.dataGridViewPhases.Name = "dataGridViewPhases";
+			this.dataGridViewPhases.RowTemplate.Height = 24;
+			this.dataGridViewPhases.Size = new System.Drawing.Size(461, 420);
+			this.dataGridViewPhases.TabIndex = 36;
+			// 
+			// PhaseViewerForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(488, 527);
+			this.Controls.Add(this.dataGridViewPhases);
+			this.Controls.Add(this.buttonShowPhase);
+			this.Controls.Add(this.buttonShowAllPhases);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "PhaseViewerForm";
+			this.Text = "Phase Viewer";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.PhaseViewerForm_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewPhases)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button buttonShowAllPhases;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button buttonShowPhase;
+		private System.Windows.Forms.DataGridView dataGridViewPhases;
+	}
+}
+
---
+++ PhaseViewer/PhaseViewerForm.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.UI;
+
+namespace PhaseViewer
+{
+	public partial class PhaseViewerForm : Form
+	{
+		private readonly Model _model = new Model();
+		private readonly Dictionary<int, Color> _phaseColors;
+		private readonly List<Part> _mainParts;
+
+		public PhaseViewerForm()
+		{
+			InitializeComponent();
+
+			_phaseColors = new Dictionary<int, Color>
+			{
+				{1, new Color(1, 0, 0)},
+				{2, new Color(0, 1, 0)},
+				{3, new Color(0, 0, 1)},
+				{4, new Color(1, 1, 0)},
+				{5, new Color(1, 1, 1)},
+				{6, new Color(0, 1, 0)},
+				{7, new Color(0, 0, 1)},
+				{13, new Color(1, 0, 1)},
+				{99, new Color(0, 1, 1)},
+				{100, new Color(0, 1, .5)},
+				{101, new Color(.5, .5, .5)},
+				{9999, new Color(.5, 0, 0)},
+				{10000, new Color(0, .5, 0)},
+			};
+
+			_mainParts = GetMainParts();
+		}
+
+		private void PhaseViewerForm_Load(object sender, EventArgs e)
+		{
+			var phases = _model.GetPhases()
+				.OfType<Phase>()
+				.ToList()
+				.Select(p => new
+				{
+					Number = p.PhaseNumber,
+					Name = p.PhaseName,
+					Comment = p.PhaseComment,
+					Current = p.IsCurrentPhase == 1 ? "CURRENT" : string.Empty
+				}).ToList();
+
+			phases.Sort((a,b) => a.Number.CompareTo(b.Number));
+
+			dataGridViewPhases.DataSource = phases;
+		}
+
+		private List<Part> GetMainParts()
+		{
+			ModelObjectEnumerator.AutoFetch = true;
+
+			var beams = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM)
+				.ToAList<Beam>();
+
+			var bentPlates = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BENT_PLATE)
+				.ToAList<BentPlate>();
+
+			var contourPlates = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.CONTOURPLATE)
+				.ToAList<ContourPlate>();
+
+			var polybeams = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.POLYBEAM)
+				.ToAList<PolyBeam>();
+
+			var parts = new List<Part>();
+
+			parts.AddRange(beams);
+			parts.AddRange(bentPlates);
+			parts.AddRange(contourPlates);
+			parts.AddRange(polybeams);
+
+			var mainParts = parts.AsParallel().Where(p =>
+			{
+				int isMainPart = 0;
+				p.GetReportProperty("MAIN_PART", ref isMainPart);
+				return isMainPart > 0;
+			}).ToList();
+
+			return mainParts;
+		}
+
+		private void buttonShowAllPhases_Click(object sender, EventArgs e)
+		{
+			var phaseGroups = _mainParts.AsParallel().GroupBy(p =>
+			{
+				p.GetPhase(out Phase phase);
+				return phase.PhaseNumber;
+			}).ToList();
+
+			phaseGroups.ForEach(p =>
+			{
+				ModelObjectVisualization.SetTemporaryState(p.OfType<ModelObject>().ToList(), _phaseColors[p.Key]);
+			});
+		}
+
+		private void buttonShowPhase_Click(object sender, EventArgs e)
+		{
+			dynamic row = dataGridViewPhases?.CurrentRow?.DataBoundItem;
+			var phaseNumber = (int)row.Number;
+
+			var modelObjects = _mainParts.AsParallel()
+				.Where(p =>
+				{
+					p.GetPhase(out Phase phase);
+					return phase.PhaseNumber == phaseNumber;
+				})
+				.OfType<ModelObject>()
+				.ToList();
+
+			ModelObjectVisualization.SetTemporaryState(modelObjects, _phaseColors[phaseNumber]);
+		}
+	}
+
+	public static class ExtensionMethods
+	{
+		public static List<T> ToAList<T>(this IEnumerator enumerator)
+		{
+			var list = new List<T>();
+			while (enumerator.MoveNext())
+			{
+				var current = enumerator.Current;
+				if (!(current is T)) continue;
+				list.Add((T)current);
+			}
+			return list;
+		}
+	}
+}
